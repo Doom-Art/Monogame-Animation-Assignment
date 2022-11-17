4cb62fc65531aa6ac0fb09058ab72e9a5da86212
Doom-Art <ouginak121@gmail.com>
2022-11-17 09:00:19 -0500

    done

--- Game1.cs
+++ Game1.cs
@@ -23,7 +23,7 @@ namespace Monogame_Animation_Assignment
         private Vector2 tribbleBrownSpeed;
         public static Random rand;
         private int bgNum;
-
+        private float rotation;
 
         public Game1()
         {
@@ -39,6 +39,7 @@ namespace Monogame_Animation_Assignment
             _graphics.PreferredBackBufferHeight = 600;
             _graphics.ApplyChanges();
             bgNum = 0;
+            rotation = 1;
             tribbleGreyRect = new Rectangle(rand.Next(0,700), rand.Next(0,500), 100, 100);
             tribbleGreySpeed = new Vector2(2, 2);
             tribbleOrangeRect = new Rectangle(rand.Next(0, 700), rand.Next(0, 500), 100, 100);
@@ -101,6 +102,7 @@ namespace Monogame_Animation_Assignment
             if (tribbleBrownRect.Right >= _graphics.PreferredBackBufferWidth || tribbleBrownRect.Left <= 0){
                 tribbleBrownSpeed.X *= (-1);
                 bgNum = rand.Next(0, 5);
+                rotation *= -1;
             }
             if (tribbleBrownRect.Bottom >= _graphics.PreferredBackBufferHeight || tribbleBrownRect.Top <= 0){
                 tribbleBrownSpeed.Y *= (-1);
@@ -121,17 +123,22 @@ namespace Monogame_Animation_Assignment
                 GraphicsDevice.Clear(Color.BlueViolet);
             else if (bgNum == 3)
                 GraphicsDevice.Clear(Color.Gold);
-            else if (bgNum == 4)
+            else if (bgNum == 4)    
                 GraphicsDevice.Clear(Color.Green);
-
 
             _spriteBatch.Begin();
             _spriteBatch.Draw(tribbleGreyTexture, tribbleGreyRect, Color.White);
             _spriteBatch.Draw(tribbleCreamTexture, tribbleCreamRect, Color.White);
             _spriteBatch.Draw(tribbleOrangeTexture, tribbleOrangeRect, Color.White);
-            _spriteBatch.Draw(tribbleBrownTexture, tribbleBrownRect, Color.White);
+            _spriteBatch.Draw(tribbleBrownTexture, tribbleBrownRect, null, Color.White, rotation, new Vector2(tribbleBrownTexture.Width / 2f, tribbleBrownTexture.Height / 2f), SpriteEffects.None, 0f);
             _spriteBatch.End();
             // TODO: Add your drawing code here
+            if (rotation > 360 || rotation < -360)
+                rotation = 1;
+            else if (rotation > 0)
+                rotation += Convert.ToSingle(0.3);
+            else if (rotation < 0)
+                rotation -= Convert.ToSingle(0.3);
 
             base.Draw(gameTime);
         }
