e943f405ab23deca25ef83a5f50557333aef03ae
Doom-Art <ouginak121@gmail.com>
2022-12-07 09:26:40 -0500

    Started part 6

---
+++ Tribble.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rectangle = Microsoft.Xna.Framework.Rectangle;
+
+namespace Monogame_Animation_Assignment
+{
+    internal class Tribble
+    {
+        private Texture2D _texture;
+        private Rectangle _rectangle;
+        private Vector2 _speed;
+    }
+}
